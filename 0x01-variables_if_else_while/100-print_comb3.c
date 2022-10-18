3c40681e3bc57bc3f9fdb41176ba77bc2465b6d4
Chinedum Egelamba <chinedumegelamba@gmail.com>
2022-10-18 13:20:45 -0700

    task 10 update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,7 +17,7 @@ int main(void)
 			{
 				putchar(tens);
 				putchar(ones);
-				if (!((ones == '9' && == '8')))/*adds comma and space*/
+				if (!(ones == '9' && == '8'))/*adds comma and space*/
 				{
 					putchar(',');
 					putchar(' ');
@@ -25,8 +25,6 @@ int main(void)
 			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
