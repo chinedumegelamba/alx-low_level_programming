3b8c97a37b6573a7597622826f893fb33100d1b2
Chinedum Egelamba <chinedumegelamba@gmail.com>
2022-10-20 15:19:02 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * islower - checks if the character is lowercase
+ * Return: returns 1 and 0 depending on condition
+ */
+int_islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
