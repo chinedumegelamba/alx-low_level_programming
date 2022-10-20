e4ceeb4bcd55ac0a0b867b66702940d999bc5625
Chinedum Egelamba <chinedumegelamba@gmail.com>
2022-10-20 12:04:39 -0700

    task hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,6 +2,10 @@
 
 /**
  * print_alphabet_10 - function to print abc 10 times
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
